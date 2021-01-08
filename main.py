7470057f1d1c5b7c8c6e7c2de0b1f48e5cdc537c
Andry <asabino512@gmail.com>
2021-01-08 15:50:29 -0300

    tentenado fazer com euro

--- main.py
+++ main.py
@@ -17,8 +17,34 @@ def getValorDolar ():
 
     return float_real
 
-res = float(input('Quantos dólares você quer converter??\nEx: U$S 2.32\n>: U$S '))
+res = float(input('Quantos dólares você quer converter? Ex: U$S 2.32\n>: U$S '))
 umDolarEmReal = getValorDolar()
 valorReal = res * umDolarEmReal
 
-print('\n\nU$S', res, ' em reais é igual a: R$ ', valorReal, '\n\n')
+print('\n\nU$S', res, ' em reais é igual a: R$ ', valorReal, '\n\n') 
+
+
+def getValorEuro ():
+    html_da_pag_euro = request.urlopen('https://www.dolarhoje.net.br/euro-hoje/')
+
+    findEur = '<input type="text" id="moeda" value="6,58"'
+
+    listaEur = (html_da_pag_euro.read().decode('utf-8')).split(findEur)
+
+    parte3 = (listaEur[1]).split()
+
+    value_htmlEur = parte3[0]
+
+    valor_realEur = (value_htmlEur.split('"'))[1]
+
+    float_realEur = float(valor_realEur.replace(',', '.'))
+   
+    return float_realEur
+
+resEur = float(input('Quantos euros você quer converter? Ex:20€\n >:€'))
+
+umEuroEmReais = getValorEuro()
+
+valorRealEur = resEur * umEuroEmReais
+
+print('\n\n€', res, 'em reais é igual a: R$', valorRealEur, '\n\n')
